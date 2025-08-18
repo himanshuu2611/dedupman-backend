018df454e8f5c8447335df17021df171ac66d7b7
himanshuu2611 <himanshutiwari6371@gmail.com>
2025-08-18 10:06:46 +0530

    update

---
+++ src/main/java/com/dedupman/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.dedupman.dedupman.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://dedupman.vercel.app/")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+        };
+    }
+}
