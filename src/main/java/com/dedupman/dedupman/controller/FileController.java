c9d537d622d1a2d3fa9adf77faff13021f9ad726
himanshuu2611 <himanshutiwari6371@gmail.com>
2025-08-18 11:25:54 +0530

    Update FileController.java

--- src/main/java/com/dedupman/dedupman/controller/FileController.java
+++ src/main/java/com/dedupman/dedupman/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.dedupman.dedupman.controller;
 
+import com.dedupman.dedupman.service.DuplicateFileRemover; // <-- THIS WAS MISSING
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
